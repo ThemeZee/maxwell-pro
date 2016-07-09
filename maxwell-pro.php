7d07f6f6f07e2bf2a22b138628eb3740ad995767
Thomas W <netzberufler@googlemail.com>
2016-07-09 16:13:35 +0200

    improved code formatting

--- maxwell-pro.php
+++ maxwell-pro.php
@@ -16,11 +16,8 @@ Copyright(C) 2016, ThemeZee.com - support@themezee.com
 
 */
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-// Use class to avoid namespace collisions
-if ( ! class_exists( 'Maxwell_Pro' ) ) :
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 
 /**
@@ -39,51 +36,51 @@ class Maxwell_Pro {
 	 * @return void
 	 */
 	static function setup() {
-	
-		// Setup Constants
+
+		// Setup Constants.
 		self::constants();
-		
-		// Setup Translation
+
+		// Setup Translation.
 		add_action( 'plugins_loaded', array( __CLASS__, 'translation' ) );
-	
-		// Include Files
+
+		// Include Files.
 		self::includes();
-		
-		// Setup Action Hooks
+
+		// Setup Action Hooks.
 		self::setup_actions();
-		
+
 	}
-	
+
 	/**
 	 * Setup plugin constants
 	 *
 	 * @return void
 	 */
 	static function constants() {
-		
-		// Define Plugin Name
+
+		// Define Plugin Name.
 		define( 'MAXWELL_PRO_NAME', 'Maxwell Pro' );
 
-		// Define Version Number
+		// Define Version Number.
 		define( 'MAXWELL_PRO_VERSION', '1.0' );
-		
-		// Define Plugin Name
+
+		// Define Plugin Name.
 		define( 'MAXWELL_PRO_PRODUCT_ID', 65584 );
 
-		// Define Update API URL
-		define( 'MAXWELL_PRO_STORE_API_URL', 'https://themezee.com' ); 
+		// Define Update API URL.
+		define( 'MAXWELL_PRO_STORE_API_URL', 'https://themezee.com' );
 
-		// Plugin Folder Path
+		// Plugin Folder Path.
 		define( 'MAXWELL_PRO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-		// Plugin Folder URL
+		// Plugin Folder URL.
 		define( 'MAXWELL_PRO_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-		// Plugin Root File
+		// Plugin Root File.
 		define( 'MAXWELL_PRO_PLUGIN_FILE', __FILE__ );
-		
+
 	}
-	
+
 	/**
 	 * Load Translation File
 	 *
@@ -92,41 +89,41 @@ class Maxwell_Pro {
 	static function translation() {
 
 		load_plugin_textdomain( 'maxwell-pro', false, dirname( plugin_basename( MAXWELL_PRO_PLUGIN_FILE ) ) . '/languages/' );
-		
+
 	}
-	
+
 	/**
 	 * Include required files
 	 *
 	 * @return void
 	 */
 	static function includes() {
-	
-		// Include Admin Classes
+
+		// Include Admin Classes.
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/admin/class-plugin-updater.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/admin/class-settings.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/admin/class-settings-page.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/admin/class-admin-notices.php';
-		
-		// Include Customizer Classes
+
+		// Include Customizer Classes.
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/customizer/class-customizer.php';
-		
-		// Include Pro Features
+
+		// Include Pro Features.
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-custom-colors.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-custom-fonts.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-footer-line.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-footer-widgets.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-header-bar.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/modules/class-header-spacing.php';
-		
-		// Include Magazine Widgets
+
+		// Include Magazine Widgets.
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/widgets/widget-magazine-posts-boxed.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/widgets/widget-magazine-posts-list.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/widgets/widget-magazine-posts-sidebar.php';
 		require_once MAXWELL_PRO_PLUGIN_DIR . 'includes/widgets/widget-magazine-posts-single.php';
 
 	}
-	
+
 	/**
 	 * Setup Action Hooks
 	 *
@@ -134,19 +131,19 @@ class Maxwell_Pro {
 	 * @return void
 	 */
 	static function setup_actions() {
-		
-		// Enqueue Maxwell Pro Stylesheet
+
+		// Enqueue Maxwell Pro Stylesheet.
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles' ), 11 );
-		
-		// Register additional Magazine Post Widgets
+
+		// Register additional Magazine Post Widgets.
 		add_action( 'widgets_init', array( __CLASS__, 'register_widgets' ) );
-		
-		// Add Settings link to Plugin actions
+
+		// Add Settings link to Plugin actions.
 		add_filter( 'plugin_action_links_' . plugin_basename( MAXWELL_PRO_PLUGIN_FILE ), array( __CLASS__, 'plugin_action_links' ) );
-		
-		// Add automatic plugin updater from ThemeZee Store API
+
+		// Add automatic plugin updater from ThemeZee Store API.
 		add_action( 'admin_init', array( __CLASS__, 'plugin_updater' ), 0 );
-		
+
 	}
 
 	/**
@@ -156,58 +153,59 @@ class Maxwell_Pro {
 	 */
 	static function enqueue_styles() {
 
-		// Return early if Maxwell Theme is not active
-		if ( ! current_theme_supports( 'maxwell-pro'  ) ) {
+		// Return early if Maxwell Theme is not active.
+		if ( ! current_theme_supports( 'maxwell-pro' ) ) {
 			return;
 		}
-		
-		// Enqueue Plugin Stylesheet
+
+		// Enqueue Plugin Stylesheet.
 		wp_enqueue_style( 'maxwell-pro', MAXWELL_PRO_PLUGIN_URL . 'assets/css/maxwell-pro.css', array(), MAXWELL_PRO_VERSION );
-		
-		// Get Custom CSS
+
+		// Get Custom CSS.
 		$custom_css = apply_filters( 'maxwell_pro_custom_css_stylesheet', '' );
-		
-		// Sanitize CSS Code
+
+		// Sanitize CSS Code.
 		$custom_css = wp_kses( $custom_css, array( '\'', '\"' ) );
 		$custom_css = str_replace( '&gt;', '>', $custom_css );
 		$custom_css = preg_replace( '/\t\t\t\t/', '', $custom_css );
-		
-		// Enqueue Custom CSS
+
+		// Enqueue Custom CSS.
 		wp_add_inline_style( 'maxwell-pro', $custom_css );
-		
+
 	}
-	
+
 	/**
 	 * Register Magazine Widgets
 	 *
 	 * @return void
 	 */
 	static function register_widgets() {
-		
-		// Return early if Maxwell Theme is not active
-		if ( ! current_theme_supports( 'maxwell-pro'  ) ) {
+
+		// Return early if Maxwell Theme is not active.
+		if ( ! current_theme_supports( 'maxwell-pro' ) ) {
 			return;
 		}
-		
+
 		register_widget( 'Maxwell_Pro_Magazine_Posts_Boxed_Widget' );
 		register_widget( 'Maxwell_Pro_Magazine_Posts_List_Widget' );
 		register_widget( 'Maxwell_Pro_Magazine_Posts_Sidebar_Widget' );
 		register_widget( 'Maxwell_Pro_Magazine_Posts_Single_Widget' );
-		
+
 	}
-	
+
 	/**
 	 * Add Settings link to the plugin actions
 	 *
+	 * @param array $actions Plugin action links.
 	 * @return array $actions Plugin action links
 	 */
 	static function plugin_action_links( $actions ) {
 
 		$settings_link = array( 'settings' => sprintf( '<a href="%s">%s</a>', admin_url( 'themes.php?page=maxwell-pro' ), __( 'Settings', 'maxwell-pro' ) ) );
-		
+
 		return array_merge( $settings_link, $actions );
 	}
-	
+
 	/**
 	 * Plugin Updater
 	 *
@@ -215,33 +213,30 @@ class Maxwell_Pro {
 	 */
 	static function plugin_updater() {
 
-		if( ! is_admin() ) :
+		if ( ! is_admin() ) :
 			return;
 		endif;
-		
+
 		$options = Maxwell_Pro_Settings::instance();
 
-		if( $options->get( 'license_key' ) <> '' ) :
-			
+		if ( $options->get( 'license_key' ) <> '' ) :
+
 			$license_key = $options->get( 'license_key' );
-			
-			// setup the updater
+
+			// Setup the updater.
 			$maxwell_pro_updater = new Maxwell_Pro_Plugin_Updater( MAXWELL_PRO_STORE_API_URL, __FILE__, array(
 					'version' 	=> MAXWELL_PRO_VERSION,
 					'license' 	=> $license_key,
 					'item_name' => MAXWELL_PRO_NAME,
 					'item_id'   => MAXWELL_PRO_PRODUCT_ID,
-					'author' 	=> 'ThemeZee'
+					'author' 	=> 'ThemeZee',
 				)
 			);
-			
+
 		endif;
-		
+
 	}
-	
 }
 
-// Run Plugin
+// Run Plugin.
 Maxwell_Pro::setup();
-
-endif;
