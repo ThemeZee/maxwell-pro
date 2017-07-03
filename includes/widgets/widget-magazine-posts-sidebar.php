3da5bbd57568a5a9fdf2d70fc74a6a5ef34352b1
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-07-03 18:59:41 +0200

    added default titles for Magazine Widgets and adjusted description

--- includes/widgets/widget-magazine-posts-sidebar.php
+++ includes/widgets/widget-magazine-posts-sidebar.php
@@ -36,10 +36,10 @@ class Maxwell_Pro_Magazine_Posts_Sidebar_Widget extends WP_Widget {
 	private function default_settings() {
 
 		$defaults = array(
-			'title'				=> '',
-			'category'			=> 0,
-			'number'			=> 4,
-			'excerpt'			=> false,
+			'title'    => esc_html__( 'Magazine (Sidebar)', 'maxwell-pro' ),
+			'category' => 0,
+			'number'   => 4,
+			'excerpt'  => false,
 		);
 
 		return $defaults;
