0699e21c4e2c5fe35134d4c7f0726f2f8511d19b
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-09-15 10:32:47 +0200

    added AMP support for secondary navigation menu

--- includes/modules/class-header-bar.php
+++ includes/modules/class-header-bar.php
@@ -8,7 +8,9 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Header Bar Class
@@ -52,15 +54,14 @@ class Maxwell_Pro_Header_Bar {
 				// Display Social Icons Menu.
 				wp_nav_menu( array(
 					'theme_location' => 'social',
-					'container' => false,
-					'menu_class' => 'social-icons-menu',
-					'echo' => true,
-					'fallback_cb' => '',
-					'link_before' => '<span class="screen-reader-text">',
-					'link_after' => '</span>',
-					'depth' => 1,
-					)
-				);
+					'container'      => false,
+					'menu_class'     => 'social-icons-menu',
+					'echo'           => true,
+					'fallback_cb'    => '',
+					'link_before'    => '<span class = "screen-reader-text">',
+					'link_after'     => '</span>',
+					'depth'          => 1,
+				) );
 
 				echo '</div>';
 
@@ -69,7 +70,7 @@ class Maxwell_Pro_Header_Bar {
 			// Check if there is a top navigation menu.
 			if ( has_nav_menu( 'secondary' ) ) : ?>
 
-				<button class="secondary-menu-toggle menu-toggle" aria-controls="secondary-menu" aria-expanded="false">
+				<button class="secondary-menu-toggle menu-toggle" aria-controls="secondary-menu" aria-expanded="false" <?php self::amp_menu_toggle(); ?>>
 					<?php
 					echo self::get_svg( 'menu' );
 					echo self::get_svg( 'close' );
@@ -79,7 +80,7 @@ class Maxwell_Pro_Header_Bar {
 
 				<div class="secondary-navigation">
 
-					<nav class="top-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Secondary Menu', 'maxwell-pro' ); ?>">
+					<nav class="top-navigation" role="navigation" <?php self::amp_menu_is_toggled(); ?> aria-label="<?php esc_attr_e( 'Secondary Menu', 'maxwell-pro' ); ?>">
 
 						<?php
 						wp_nav_menu(
@@ -127,9 +128,27 @@ class Maxwell_Pro_Header_Bar {
 
 		register_nav_menus( array(
 			'secondary' => esc_html__( 'Top Navigation', 'maxwell-pro' ),
-			'social' => esc_html__( 'Social Icons', 'maxwell-pro' ),
+			'social'    => esc_html__( 'Social Icons', 'maxwell-pro' ),
 		) );
+	}
 
+	/**
+	 * Adds amp support for menu toggle.
+	 */
+	static function amp_menu_toggle() {
+		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+			echo "[aria-expanded]=\"secondaryMenuExpanded? 'true' : 'false'\" ";
+			echo 'on="tap:AMP.setState({secondaryMenuExpanded: !secondaryMenuExpanded})"';
+		}
+	}
+
+	/**
+	 * Adds amp support for mobile dropdown navigation menu.
+	 */
+	static function amp_menu_is_toggled() {
+		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+			echo "[class]=\"'top-navigation' + ( secondaryMenuExpanded ? ' toggled-on' : '' )\"";
+		}
 	}
 }
 
